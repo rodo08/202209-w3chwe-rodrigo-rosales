b0afcea0b0aa6da86d90bc4904b3fb0804586c2f
rodo08 <rodrigo.rosalesmoya@gmail.com>
2022-10-16 11:22:45 +0200

    Add pokeApi link up files

---
+++ src/pokeApi-linkup/urls.ts
@@ -0,0 +1,6 @@
+export const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+const pokeListNumber = 10;
+const pokeOffsetNumber = 0;
+export const pokeListTotal = `?limit=${pokeListNumber}`;
+export const pokeOffsetTotal = `&offset=${pokeOffsetNumber}`;
--- src/url.ts
+++
@@ -1,6 +0,0 @@
-const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
-const showingLimit = "?limit=10";
-
-const listLimit = `${apiUrl}${showingLimit}`;
-
-export default listLimit;
